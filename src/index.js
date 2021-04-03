fdea294b264aed46ca71f7ce4b63938b38e18d88
Julia Collins <collins.acumed@gmail.com>
2021-04-02 17:29:21 -0700

    adding to favorites and displaying favorites routes working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-const { getToken, getDogData, receiveUserData } = require('./middleware');
+const { getToken, getDogData, receiveUserData, displayUserFavs } = require('./middleware');
 const db = require('./db/queries')
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,10 +25,13 @@ app.post('/addDog/:id', db.postDog)
 
 //manipulate or access user data
 app.get('/users/:id', db.getUserById)
+app.get('/userDogs/:id', db.getDogsById)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 app.get('/users', db.getUsers)
 app.post('/users/:user_id/dogs/:dog_id', db.addFavorite)
+app.get('/userFavs/:id', getToken, db.getUserFavorites, displayUserFavs)
+
 
 //just using this route to test posted dogs (temporary)
 app.get('/getDogs', db.getPostedDogs)
